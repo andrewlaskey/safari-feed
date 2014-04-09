102478a02527009b9dfaefda1c2e71f7d16f9a15
Andrew Laskey <andrew.laskey@gmail.com>
2014-04-08 21:30:36 -0700

    added marker update

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -3,15 +3,22 @@ var safariFeedControllers = angular.module('safariFeedControllers', []);
 safariFeedControllers.controller('MapCtrl', ['$scope', '$firebase', 'mapService',
   function ($scope, $firebase, mapService) {
 
+  	mapService.loadMap();
+
     //Get Zoo Data
 	$scope.zoo = $firebase(new Firebase('https://safarifeed.firebaseio.com/zoos/0'));
+	$scope.updates = $firebase(new Firebase('https://safarifeed.firebaseio.com/zoos/0/updates'));
 	
 	//Load map when ready
 	$scope.zoo.$on('loaded', function(value) {
-
-		mapService.loadMap(value.center, value.bounds);
+		mapService.getMapReady(value.center, value.bounds);
 		mapService.addHeatMap(value.updates);
-		mapService.addRecentUpdates(value.updates);
+		
+	});
+
+	$scope.updates.$on('child_added', function(value) {
+		var blob = value.snapshot.value;
+		mapService.addUpdate(blob);
 	});
 	
   }]);
--- www/js/services.js
+++ www/js/services.js
@@ -6,13 +6,15 @@ service('mapService', function() {
 
 	var map, updateMarkers = [];
 
-	this.loadMap = function(zooCenter, zooBounds) {
-
+	this.loadMap = function() {
 		map = L.map('map');
 
 		L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
 	    	attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 		}).addTo(map);
+	};
+
+	this.getMapReady = function(zooCenter, zooBounds) {
 
 		map.setView(zooCenter.split(','), 15);
 
@@ -30,6 +32,7 @@ service('mapService', function() {
 	};
 
 	this.addHeatMap = function(updates) {
+
 		var heatmapLayer = new L.TileLayer.HeatCanvas({},{
 			'step': 0.5,
 	        'degree': HeatCanvas.LINEAR,
@@ -56,38 +59,37 @@ service('mapService', function() {
 		L.control.layers(null, {'Historic Activity': heatmapLayer}, {collapsed: false}).addTo(map);
 	};
 
-	this.addRecentUpdates = function(updates) {
-		
+	this.addUpdate = function(update) {
+
 		//We only want updates from the last hour
 		var testTime = new Date();
 		testTime.setMinutes(testTime.getMinutes() - 60);
 
-		_.each(updates, function(value, key) {
-			if (value.time >= testTime.valueOf()) {
+		if (update.time >= testTime.valueOf()) {
 
-				if (typeof value.loc.latitude !== 'undefined' &&
-					typeof value.loc.longitude !== 'undefined') {
+			if (typeof update.loc.latitude !== 'undefined' &&
+				typeof update.loc.longitude !== 'undefined') {
 
-					var iconMarker = L.AwesomeMarkers.icon({
-						icon: 'smiley',
-						markerColor: 'green'
+				var iconMarker = L.AwesomeMarkers.icon({
+					icon: 'smiley',
+					markerColor: 'green'
+				});
+
+				if (update.sentiment === 'neg') {
+					iconMarker = L.AwesomeMarkers.icon({
+						icon: 'sad',
+						markerColor: 'darkred'
 					});
-
-					if (value.sentiment === 'neg') {
-						iconMarker = L.AwesomeMarkers.icon({
-							icon: 'sad',
-							markerColor: 'darkred'
-						});
-					}
-
-					var marker = L.marker([value.loc.latitude, value.loc.longitude], {icon: iconMarker})
-						.bindPopup('<strong>' + value.comment + '</strong><br /><i>' + moment(value.time).format("M/D/YY, h:mm a") + '</i>')
-					
-					updateMarkers.push(marker);
-					updateMarkers[updateMarkers.length - 1].addTo(map);
 				}
+
+				var marker = L.marker([update.loc.latitude, update.loc.longitude], {icon: iconMarker})
+					.bindPopup('<strong>' + update.comment + '</strong><br /><i>' + moment(update.time).format("M/D/YY, h:mm a") + '</i>')
+				console.log();
+				updateMarkers.push(marker);
+				
+				updateMarkers[updateMarkers.length - 1].addTo(map);
 			}
-		});
+		}
 	};
 
 });
