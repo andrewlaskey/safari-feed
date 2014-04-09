fe9ea201daba8f370586ae8a9a73db1fd349f0a8
Andrew Laskey <andrew.laskey@gmail.com>
2014-04-08 19:45:57 -0700

    updated map controller

---
+++ www/js/controllers.js
@@ -0,0 +1,16 @@
+var safariFeedControllers = angular.module('safariFeedControllers', []);
+ 
+safariFeedControllers.controller('MapCtrl', ['$scope', '$firebase', 'mapService',
+  function ($scope, $firebase, mapService) {
+
+    //Get Zoo Data
+	$scope.zoo = $firebase(new Firebase('https://safarifeed.firebaseio.com/zoos/0'));
+	
+	//Load map when ready
+	$scope.zoo.$on('loaded', function(value) {
+
+		mapService.loadMap(value.center, value.bounds);
+		mapService.addHeatMap(value.updates);
+	});
+	
+  }]);
