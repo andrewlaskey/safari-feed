1e76046e3c7847f64f01829443714ed3af240864
Andrew Laskey <andrew.laskey@gmail.com>
2014-05-06 21:12:54 -0700

    reorganized files and folders

--- gulpfile.js
+++ gulpfile.js
@@ -8,7 +8,7 @@ var autoprefixer = require('gulp-autoprefixer');
 var uglify = require('gulp-uglify');
 
 var paths = {
-  sass: ['./www/css/sass/**/*.scss'],
+  sass: ['./src/css/sass/**/*.scss'],
   js: [
     './app/_bower_components/angularjs-bower/angular.min.js',
     './app/_bower_components/angularjs-bower/angular-route.min.js',
@@ -19,11 +19,19 @@ var paths = {
     './app/_bower_components/momentjs/min/moment.min.js',
     './app/_bower_components/fastclick/lib/fastclick.js',
     './app/_bower_components/underscore/underscore.js',
-    './www/js/libs/geoposition.js',
-    './www/js/libs/heatcanvas.js',
-    './www/js/app.js',
-    './www/js/controllers.js',
-    './www/js/services.js'
+    './src/js/libs/geoposition.js',
+    './src/js/libs/heatcanvas.js',
+    './src/js/app.js',
+    './src/js/controllers.js',
+    './src/js/services.js'
+  ],
+  moveFiles: [
+    './src/fonts/*.*',
+    './src/images/*.*',
+    './src/views/*.*',
+    './src/index.html',
+    './src/js/build/scripts.min.js',
+    './src/css/style.min.css'
   ]
 };
 
@@ -31,26 +39,26 @@ gulp.task('sass', function(done) {
   gulp.src(paths.sass)
     .pipe(sass())
     .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
-    .pipe(gulp.dest('./www/css/'))
+    .pipe(gulp.dest('./src/css/'))
     .pipe(minifyCss({
       keepSpecialComments: 0
     }))
     .pipe(rename({ extname: '.min.css' }))
-    .pipe(gulp.dest('./www/css/'))
+    .pipe(gulp.dest('./src/css/'))
     .on('end', done);
 });
 
 gulp.task('scripts', function() {
   gulp.src(paths.js)
     .pipe(concat('scripts.js'))
-    .pipe(gulp.dest('./www/js/'))
+    .pipe(gulp.dest('./src/js/'))
     .pipe(uglify())
     .pipe(rename({ extname: '.min.js' }))
-    .pipe(gulp.dest('./www/js/build/'));
+    .pipe(gulp.dest('./src/js/build/'));
 });
 
 gulp.task('reload', function() {
-  gulp.src('./www/*.html')
+  gulp.src('./src/*.html')
     .pipe(connect.reload());
 });
 
@@ -58,9 +66,9 @@ gulp.task('watch', function() {
   gulp.watch(paths.sass, ['sass']);
   gulp.watch(paths.js, ['scripts']);
   gulp.watch([
-    './www/*.html',
-    './www/css/*.css',
-    './www/js/*.js'
+    './src/*.html',
+    './src/css/*.css',
+    './src/js/*.js'
     ], ['reload']);
 });
 
@@ -70,4 +78,11 @@ gulp.task('connect', function() {
   });
 });
 
+gulp.task('move', function() {
+  gulp.src(paths.moveFiles, { base: './src/' })
+    .pipe(gulp.dest('./dist'));
+});
+
 gulp.task('default', ['connect', 'watch']);
+
+gulp.task('deploy', ['move']);
